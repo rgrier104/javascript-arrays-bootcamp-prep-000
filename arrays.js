55b76525a76cfca9b2e31e977df0c1f45f44693b
Rachel Grier <rgrier104@gmail.com>
2018-07-30 03:20:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,3 +39,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.pop()
   return array
 }
+
+function removeElementFromEndOfArray(array) {
+  array.slice(0,array.length - 1)
+  return array
+}
