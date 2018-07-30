d9adf98b177b74f191202afbc308b4414c209706
Rachel Grier <rgrier104@gmail.com>
2018-07-30 03:35:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-    array = [element, ...array]
+    var array = [element, ...array]
     return array
 }
 
